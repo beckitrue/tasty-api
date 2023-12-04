0b04996a1651503d6b207bd9724806b4862c4381
beckitrue <3496219+beckitrue@users.noreply.github.com>
2023-12-03 21:21:52 -0600

    started breaking into funcs

--- httpclient/main.go
+++ httpclient/main.go
@@ -6,16 +6,67 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
+	"os/exec"
 )
 
-func main() {
+// API structs
+type apiMsg struct {
+	method string
+	msg    string
+}
 
-	username := os.Getenv("TT_USER")
-	session_token := os.Getenv("TT_SESSION")
+func getCreds() (string, string) {
+	// get the credentials for the Tastytrade API stored in
+	// 1Password Vault
 
-	requestURL := "https://api.cert.tastyworks.com/customers/me"
+	// set the values for 1Password CLI secret references
+	const (
+		op            = "/usr/bin/op"
+		sbxVaultUser  = "op://Private/tastytrade-sbx-api/username"
+		sbxVaultToken = "op://Private/tastytrade-sbx-api/credential"
+	)
+	user_ref, err := exec.Command(op, "read", sbxVaultUser).Output()
+
+	if err != nil {
+		log.Fatal("can't read secret reference for username ", err)
+	}
+
+	token_ref, err := exec.Command(op, "read", sbxVaultToken).Output()
+
+	if err != nil {
+		log.Fatal("can't read secret reference for api token ", err)
+	}
+
+	username := string(user_ref[:])
+	token := string(token_ref[:])
+
+	return username, token
+}
+
+func createURL(token string, env string, message apiMsg) {
+
+	const (
+		sbxURL  string = "https://api.cert.tastyworks.com/"
+		prodURL string = "https://api.tastyworks.com/"
+	)
+
+	if env == "sbx" {
+		requestURL := sbxURL + message.msg
+		fmt.Println(requestURL)
+		apiCall(token, requestURL, "GET")
+	}
+
+}
+
+func apiCall(token string, requestURL string, request string) {
+
+	// // requestURL := "https://api.cert.tastyworks.com/customers/me"
+	// req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+
+	fmt.Printf("token at client: %s\n", token)
+
 	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+
 	if err != nil {
 		log.Fatalf("client: could not create request: %s\n", err)
 	}
@@ -23,7 +74,7 @@ func main() {
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("User-Agent", "my-custom-client/2.0")
-	req.Header.Add("Authorization", session_token)
+	req.Header.Add("Authorization", token)
 
 	// need to ignore cert in the Tastytrade sandbox
 	config := tls.Config{
@@ -32,9 +83,9 @@ func main() {
 	tr := &http.Transport{TLSClientConfig: &config}
 	client := &http.Client{Transport: tr}
 
-	// client := &http.Client{
-	// 	Timeout: 10 * time.Second,
-	// }
+	// // client := &http.Client{
+	// // 	Timeout: 10 * time.Second,
+	// // }
 
 	res, err := client.Do(req)
 
@@ -50,4 +101,23 @@ func main() {
 		log.Fatalf("client: could not read response body: %s\n", err)
 	}
 	fmt.Printf("client: response body: %s\n", resBody)
+
+}
+
+func main() {
+
+	const (
+		env = "sbx"
+	)
+
+	// get session token
+	username, token := getCreds()
+
+	fmt.Printf("Hello %s here's your session token: %s\n", username, token)
+
+	// get my account info
+	accountInfo := apiMsg{method: "GET", msg: "customer/me"}
+
+	createURL(token, env, accountInfo)
+
 }
