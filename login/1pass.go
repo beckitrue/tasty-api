69bf99ca098fe3d302b44f990aa7525832540e19
beckitrue <3496219+beckitrue@users.noreply.github.com>
2023-12-16 19:46:51 -0600

    refactored to add initial login to get session token

--- login/1pass.go
+++ login/1pass.go
@@ -1,6 +1,7 @@
 package login
 
 import (
+	"example/user/tasty/httpclient"
 	"log"
 	"os/exec"
 	"strings"
@@ -8,31 +9,74 @@ import (
 
 // 1 Password secret references
 const (
-	op               = "/usr/bin/op"
-	sbxVaultUser     = "op://Private/tastytrade-sbx-api/username"
-	sbxVaultToken    = "op://Private/tastytrade-sbx-api/credential"
-	sbxRememberToken = "op://Private/tastytrade-sbx-api/remember"
+	op = "/usr/bin/op"
+
+	sbxUserName   = "op://Private/Tasty_sbx/username"
+	sbxPassword   = "op://Private/Tasty_sbx/credential"
+	sbxVaultUser  = "op://Private/tastytrade-sbx-api/username"
+	sbxVaultToken = "op://Private/tastytrade-sbx-api/credential"
+	sbxApiItem    = "tastytrade-sbx-api"
+
+// sbxRememberToken = "op://Private/tastytrade-sbx-api/remember"
 )
 
-func GetCreds() (string, string) {
+func TrimNewLine(value string) (cleanString string) {
+
+	cleanString = strings.TrimSuffix(value, "\n")
+
+	return cleanString
+
+}
+
+func GetCreds(userRef string, passwordRef string) (string, string) {
 	// get the credentials for the Tastytrade API stored in
 	// 1Password Vault
-	user_ref, err := exec.Command(op, "read", sbxVaultUser).Output()
+	user_ref, err := exec.Command(op, "read", userRef).Output()
 
 	if err != nil {
 		log.Fatal("can't read secret reference for username ", err)
 	}
 
-	token_ref, err := exec.Command(op, "read", sbxVaultToken).Output()
+	token_ref, err := exec.Command(op, "read", passwordRef).Output()
 
 	if err != nil {
 		log.Fatal("can't read secret reference for api token ", err)
 	}
 
 	username := string(user_ref[:])
+	username = TrimNewLine(username)
+
 	token := string(token_ref[:])
-	// trim the new line from the token value before returning
-	token = strings.TrimSuffix(token, "\n")
+	token = TrimNewLine(token)
 
 	return username, token
+}
+
+func WriteCreds(user string, sessionToken string) {
+	// writes the session token to 1Password to be used for
+	// API calls "op://Private/tastytrade-sbx-api/credential"
+
+	// craft credential string
+	credential := "credential=" + sessionToken
+	// fmt.Printf("credential field: %s\n", credential)
+
+	_, err := exec.Command(op, "item", "edit", sbxApiItem, credential).Output()
+
+	if err != nil {
+		log.Fatal("can't edit api session token credential ", err)
+	}
+
+}
+
+func GetSessionToken() {
+	// login to get the session and remember tokens
+	login, password := GetCreds(sbxUserName, sbxPassword)
+
+	// trim the new line from the login value before returning
+	login = strings.TrimSuffix(login, "\n")
+
+	sessionToken := httpclient.GetSessionTokens(login, password)
+
+	// write session token to 1Password
+	WriteCreds(login, sessionToken)
 }
